b68a431625b3cb327d8b0171f5b2d31e84b4f39a
Patrick Ehlert <patrick.ehlert@europeana.eu>
2020-09-21 15:15:51 +0200

    Add sock proxy support

--- src/main/java/eu/europeana/api/myapi/MyApiApplication.java
+++ src/main/java/eu/europeana/api/myapi/MyApiApplication.java
@@ -1,5 +1,7 @@
 package eu.europeana.api.myapi;
 
+import eu.europeana.api.myapi.config.SocksProxyConfig;
+import eu.europeana.api.myapi.util.SocksProxyActivator;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,6 +25,9 @@ public class MyApiApplication extends SpringBootServletInitializer {
                     System.getenv("CF_INSTANCE_INDEX"),
                     System.getenv("CF_INSTANCE_GUID"),
                     System.getenv("CF_INSTANCE_IP"));
+
+        // Activate socks proxy (if your application requires it)
+        SocksProxyActivator.activate(new SocksProxyConfig("myapi.properties", "myapi.user.properties"));
 
         SpringApplication.run(MyApiApplication.class, args);
     }
